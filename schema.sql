18e7377ba681b78ee9a6a89d87be322fe2a835f6
ingalls <nick@ingalls.ca>
2020-02-03 22:44:53 -0500

    Web UI Frameworking

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE session (
     sid varchar NOT NULL COLLATE "default",
     sess json NOT NULL,
     expire timestamp(6) NOT NULL
 ) WITH (OIDS=FALSE);
+
+CREATE TABLE company (
+    id          UUID PRIMARY KEY,
+    addr        TEXT,
+    city        TEXT,
+    state       TEXT,
+    postcode    TEXT,
+    agents      JSONB
+)
